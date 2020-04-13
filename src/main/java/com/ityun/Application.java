33c065491f4eb0f38f7b89dc013e618f0d938372
changyuyun <969842624@qq.com>
2020-04-13 15:20:35 +0800

    增加启动文件 应用配置文件 banner.txt

---
+++ src/main/java/com/ityun/Application.java
@@ -0,0 +1,16 @@
+package com.ityun;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * project run
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "com.ityun.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
