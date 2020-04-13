13c5cca959007b3aced115d266e6259c7e0bb364
changyuyun <969842624@qq.com>
2020-04-13 21:06:42 +0800

    增加项目模块规划

--- src/main/java/com/ityun/Application.java
+++ src/main/java/com/ityun/Application.java
@@ -1,6 +1,5 @@
 package com.ityun;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * project run
  */
 @SpringBootApplication
-@MapperScan(basePackages = "com.ityun.mapper")
+//@MapperScan(basePackages = "com.ityun.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/ityun/web/controller/site/IndexController.java
+++ src/main/java/com/ityun/web/controller/site/IndexController.java
@@ -1,13 +1,13 @@
 package com.ityun.web.controller.site;
 
 import com.ityun.web.controller.BaseController;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController extends BaseController {
     @RequestMapping(value = {"/", "/index"})
     public String home() {
-        return "templates/classic/index";
+        return "string";
     }
 }
