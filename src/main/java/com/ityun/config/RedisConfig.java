41528d08814cca415dbff24cb2b567ebcfbb3b59
changyuyun <969842624@qq.com>
2020-04-16 17:59:30 +0800

    实现redis存储正确的json

--- src/main/java/com/ityun/config/RedisConfig.java
+++ src/main/java/com/ityun/config/RedisConfig.java
@@ -1,10 +1,5 @@
 package com.ityun.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,86 +8,28 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableCaching//启用缓存
-public class RedisConfig extends CachingConfigurerSupport {
+public class RedisConfig  {
 
     @Bean
-    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(mapper);
-
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        //使用StringRedisSerializer来序列化和反序列化redis的key值
+
+        // key-value结构序列化数据结构
         template.setKeySerializer(stringRedisSerializer);
-        // hash的key也采用String的序列化方式
-        template.setHashKeySerializer(stringRedisSerializer);
-        // value序列化方式采用jackson
         template.setValueSerializer(jackson2JsonRedisSerializer);
-        // hash的value序列化方式采用jackson
+        // hash数据结构序列化方式,必须这样否则存hash 就是基于jdk序列化的
+        template.setHashKeySerializer(stringRedisSerializer);
         template.setHashValueSerializer(jackson2JsonRedisSerializer);
-        template.afterPropertiesSet();
+        // 启用默认序列化方式
+        template.setEnableDefaultSerializer(true);
+
+        template.setDefaultSerializer(jackson2JsonRedisSerializer);
+        //template.afterPropertiesSet();
         return template;
-    }
-    /**
-     * 对hash类型的数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForHash();
-    }
-
-    /**
-     * 对redis字符串类型数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForValue();
-    }
-
-    /**
-     * 对链表类型的数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForList();
-    }
-
-    /**
-     * 对无序集合类型的数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForSet();
-    }
-
-    /**
-     * 对有序集合类型的数据操作
-     *
-     * @param redisTemplate
-     * @return
-     */
-    @Bean
-    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForZSet();
     }
 
 }
--- src/main/java/com/ityun/web/controller/BaseController.java
+++ src/main/java/com/ityun/web/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.ityun.web.controller;
 
+import com.google.gson.Gson;
 import com.ityun.base.lang.Result;
+import com.ityun.base.utils.RedisUtils;
 import com.ityun.base.utils.TokenUtils;
 import com.ityun.modules.entity.User;
 import com.ityun.modules.service.UserService;
@@ -14,21 +16,24 @@ public class BaseController {
     @Autowired
     private UserService userService;
 
+    private Long expiredTime = 3600L;
+
     protected Result executeLogin(String username, String password) {
-        //TODO:实现token的过期策略
         Map<String, String> result = new HashMap<>();
         User user = userService.login(username, md5(password));
-        System.out.println(user);
         if (user == null) {
             return Result.failure("username or password is error");
         }
+        String token = TokenUtils.makeToken();
         result.put("username", user.getUsername());
         result.put("name", user.getName());
         result.put("email", user.getEmail());
         result.put("avatar", user.getAvatar());
         result.put("gender", String.valueOf(user.getGender()));
         result.put("last_login", String.valueOf(user.getLast_login()));
-        result.put("token", TokenUtils.makeToken());
+        result.put("token", token);
+        result.put("expiredTime", String.valueOf(expiredTime));
+        tokenStore(token, result, expiredTime);
         return Result.success("ok", result);
     }
 
@@ -36,6 +41,8 @@ public class BaseController {
         return DigestUtils.md5Hex(needle).toUpperCase();
     }
 
-
+    protected boolean tokenStore(String token, Object data, Long expiredTime) {
+        return RedisUtils.set(token, data, expiredTime);
+    }
 
 }
